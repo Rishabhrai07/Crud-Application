74e2bf83986bad205bde46e7b742d19a4476f5a5
Rishabh Rai <rishabh8403@gmail.com>
2023-02-02 21:52:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import CrudApp from './components/CrudApp';
+import AllUsers from './components/AllUsers';
+import AddUsers from './components/AddUsers';
+
+//TO ENABLE ROUTER IN REACT APP FIRST
+/*NPM INSTALL REACT-ROUTER-DOM
+
+THEN we can enable router here by importing browserRouter
+
+AND ROUTES---> IS USED IN THIS ALSO TO TOGGLE BETEEN OUR COMPONENTS .
+AND ROUTE---> IS USED TO GIVE THE PATH NAME TO ON WHICH COMPONENT WE WANT TO GO. 
+*/
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import EditUser from './components/EditUser';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+      <Route path='/' element = {<CrudApp/>} />
+      <Route path='/all' element = {<AllUsers/>} />
+      <Route path='/add' element = {<AddUsers/>} />
+      <Route path= '/edit/:id' element ={<EditUser/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
